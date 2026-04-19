bf335d6c807d3eb32c6b5ef7dbfb20068b2b9a3b
joehindle <hindlejoe2002@gmail.com>
2026-04-19 13:36:30 +0100

    Lesson1Complete

---
+++ Lesson1-Website/index.js
@@ -0,0 +1,5 @@
+console.log(`Hello World!`);
+console.log(`This is my first JavaScript website!`);
+
+document.getElementById("myh1").innerText = "Hello JavaScript!"; 
+document.getElementById("myp").innerText = "This is a paragraph that I changed with JavaScript!";
